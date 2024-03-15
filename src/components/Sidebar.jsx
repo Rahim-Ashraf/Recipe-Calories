e30eb1c42a500d112f04ecd27cf0d5140b9ab7db
Abdur Rahim <rahim11500000@gmail.com>
2024-03-16 01:49:27 +0600

    fixed banner image issue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,5 @@
 
 const Sidebar = ({ recipeItems }) => {
-    console.log(recipeItems)
     return (
         <div className="lg:w-2/5 p-4 border border-gray-200 rounded-xl">
             <h2 className="text-xl font-bold text-center mb-2">Want to cook: {recipeItems.length}</h2>
@@ -22,7 +21,7 @@ const Sidebar = ({ recipeItems }) => {
                                 return (
                                     <tr key={idx}>
                                         <td><p className="p-2">{idx + 1}</p></td>
-                                        <td className="p-2">z{recipeItem.recipe_name}</td>
+                                        <td className="p-2">{recipeItem.recipe_name}</td>
                                         <td className="p-2"></td>
                                         <td className="p-2"></td>
                                         <td><button className="btn rounded-full bg-[#0BE58A]">Preparing</button></td>
